75b826df3a937b706f77d985d3cf644832edadd1
Célestin Ballèvre <celestin.ballevre@protonmail.com>
2026-02-23 14:49:44 +0100

    refactor: Move v5.1 into main program SensOcean-ESP32

--- software/firmware/SensOcean-ESP32/SensOcean-ESP32.ino
+++ software/firmware/SensOcean-ESP32/SensOcean-ESP32.ino
@@ -19,49 +19,57 @@ Lipobabysister                                      - Connexion I2C
 Branchement : 
 -------------
 WaveShare display 4,2 inch on ESP32 Firebeetle
-Branchement Ecran : BUSY -> 4, RST -> 25, DC -> 13, CS -> SS(5), CLK -> SCK(18), DIN -> MOSI(23), GND -> GND, 3.3V -> 3.3V,
+Ecran : BUSY -> 4, RST -> 25, DC -> 13, CS -> SS(5), CLK -> SCK(18), DIN -> MOSI(23), GND -> GND, 3.3V -> 3.3V,
 
+Remarque : attention pour les lib ecran : penser a changer les point cpp en .h
 
 Etat du programme : 
 -------------------
-
+oki version fonctionnel
 
 Pour la prochain version : 
 ------------------------
+ajouter format SD card intro
+Gerer les allumages/veille de tout les composants
+ajouter les branchement
 
 ***********************************************************/
 
 
 // ---------------------   PARAMETRES MODIFIABLE DU PROGRAM    -----------------------------------
 // Version et numero de serie
-char numserie[] = "AESO18004";      // Numero de serie de la sonde
-char versoft[] = "5.0";             // version du code
+char numserie[] = "AESO19004";      // Numero de serie de la sonde
+char versoft[] = "5.1";             // version du code
 
 #define TIME_TO_SLEEP  10           // Durée d'endormissement entre 2 cycles complets de mesures (in seconds)
-int nbrMes = 3;                     // nombre de mesure de salinité et température par cycle
+int nbrMes = 1;                     // nombre de mesure de salinité et température par cycle
 
 // --------------------     FIN DES PARAMETRES MODIFIABLES     ------------------------------------
 
 
 // Library
-#include <GxEPD.h>
-#include <GxGDEW042T2/GxGDEW042T2.h>  // 4.2" b/w
-#include <Fonts/FreeMonoBold9pt7b.h>
-#include <Fonts/FreeMonoBold12pt7b.h>
-#include <Fonts/FreeMonoBold18pt7b.h>
-#include <Fonts/FreeMonoBold24pt7b.h>
-#include <GxIO/GxIO_SPI/GxIO_SPI.h>
-#include <GxIO/GxIO.h>
-#include <Wire.h>                    //enable I2C.
-#include <DS3231.h>                  // Pour horloge RTC
-#include "SPI.h"                     // pour connection ecran bus SPI
-#include <SD.h>                       // pour carte SD
+#include <GxEPD.h>                      // Epaper Screen
+//#include <GxGDEW042T2/GxGDEW042T2.h>    // Epaper Screen 4.2" b/w
+#include <GxGDEH029A1/GxGDEH029A1.h>    // Epaper Screen 2.9" b/w
+#include <Fonts/FreeMonoBold9pt7b.h>    // font for epaper sreen
+#include <Fonts/FreeMonoBold12pt7b.h>   // font for epaper sreen
+#include <Fonts/FreeMonoBold18pt7b.h>   // font for epaper sreen
+#include <Fonts/FreeMonoBold24pt7b.h>   // font for epaper sreen
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>     // epaper sceeen
+#include <GxIO/GxIO.h>                  // epaper screen
+#include <Wire.h>                       //enable I2C.
+#include <DS3231.h>                     // Pour horloge RTC
+#include "SPI.h"                        // pour connection ecran bus SPI
+#include <SD.h>                         // pour carte SD
+#include <SparkFunBQ27441.h>            // Batterie fuel gauge lipo babysister
+#include <TinyGPS++.h>                  // Gps
+
 
 //SPI pin definitions pour ecran epaper
 GxIO_Class io(SPI, /*CS=5*/ 0, /*DC=*/ 13, /*RST=*/ 25); // arbitrary selection of 17, 16  //SS remplacé par 0
 GxEPD_Class display(io, /*RST=*/ 25, /*BUSY=*/ 4); // arbitrary selection of (16), 4
 
-// définition pour les font ecran
+// définition pour les fonts ecran
 const GFXfont* f1 = &FreeMonoBold9pt7b;
 const GFXfont* f2 = &FreeMonoBold12pt7b;
 const GFXfont* f3 = &FreeMonoBold18pt7b;
@@ -103,23 +111,23 @@ bool PM;
 //byte year, month, date, DoW, hour, minute, second;
 int second,minute,hour,date,month,year; 
 
+// Set BATTERY_CAPACITY to the design capacity of your battery.
+const unsigned int BATTERY_CAPACITY = 3400; // e.g. 3400mAh battery
+
+// definition pour GPS
+TinyGPSPlus gps;                           
+HardwareSerial Serial1(1);        
+
+
 void setup()
 {
   // ------------------        HERE IS NEEDED THE PARAMETER FOR THE ENTIER PROGRAMM   ------------------------------------------
-  Serial.begin(115200);
-  Wire.begin();  
-  delay(1000); //Take some time to open up the Serial Monitor
-  display.init(115200); // enable diagnostic output on Serial
-
-  // Test carte SD
-  Serial.print("Initializing SD card...");   
-    if (!SD.begin(5)) {                      // // see if the card is present and can be initialized, ajouter ici chipSelect ou 5 pour la pin 5 par default
-    Serial.println("Card failed, or not present");
-    //delay(300);
-    // don't do anything more:
-    return;
-  }
-  Serial.println("card initialized.");
+  Serial.begin(115200);                        // communication PC
+  Serial1.begin(9600, SERIAL_8N1, 16, 17);     // communication serie avec GPS
+  Wire.begin();                                // for I2C communication
+  setupBQ27441();                              // for Lipo Babysister sparkfun lipo fuelgauge
+  display.init();                              // enable display Epaper
+  delay(500); //Take some time to open up the Serial Monitor and enable all things
 
 //  pinMode(rtdpin, OUTPUT);            // pin temperature
 //  pinMode(ecpin, OUTPUT);            // pin EC
@@ -131,7 +139,8 @@ void setup()
       // --------------        HERE IS ONLY THE INTRODUCION           -----------------------------------------------------------
       
       affichageintro();       // texte d'intro et cadre initiale
-      delay(2000);
+      Serial.println(" Phase d'introduction ");
+      //delay(3000);
 
       
       bootCount = bootCount+1;   // changement du numéro de compteur pour passer directement dans programm loop apres le reveil
@@ -141,9 +150,21 @@ void setup()
   {
       // ---------------        HERE IS THE MAIN PROGRAMM LOOP         ----------------------------------------------------------
       
-//      digitalWrite(rtdpin, HIGH);   // temp
+//      digitalWrite(rtdpin, HIGH);   // temp  si allumage des pin
 //      digitalWrite(ecpin, HIGH);   // ec
 //      delay(2000);
+
+      
+
+      // Test carte SD
+      Serial.print("Initializing SD card...");   
+        if (!SD.begin(5)) {                      // // see if the card is present and can be initialized, ajouter ici chipSelect ou 5 pour la pin 5 par default
+        Serial.println("Card failed, or not present");
+        //delay(300);
+        // don't do anything more:
+        return;
+      }
+      Serial.println("card initialized.");
 
       // lecture de l'horloge rtc
       int second,minute,hour,date,month,year; 
@@ -153,14 +174,25 @@ void setup()
       date=Clock.getDate();
       month=Clock.getMonth(Century);
       year=Clock.getYear();
-
       
-      // Mesure de temp et conduc et construction de la chaine de donnée pour enregistrer sur la carte SD
+      // Read battery stats from the BQ27441-G1A
+      unsigned int soc = lipo.soc();  // Read state-of-charge (%)
+      unsigned int volts = lipo.voltage(); // Read battery voltage (mV)
+
+      //Read the gps
+      smartDelay(1000);  
+      if (millis() > 5000 && gps.charsProcessed() < 10)
+        Serial.println(F("No GPS data received: check wiring")); 
+
+          
+      // MESURE TEMPERATURE ET CONDUCTIVITE et construction de la chaine de donnée pour enregistrer sur la carte SD
       String datachain ="";
 
-        // date heure GPS de debut de chaine
-        datachain += year; datachain += " ; ";datachain += month; datachain += " ; ";datachain += date; datachain += " ; ";
-        datachain += hour; datachain += " ; ";datachain += minute; datachain += " ; ";
+        // date heure GPS bat de debut de chaine
+        datachain += gps.location.lat(); datachain += " ; "; datachain += gps.location.lng(); datachain += " ; ";
+        datachain += year; datachain += " ; "; datachain += month; datachain += " ; "; datachain += date; datachain += " ; ";
+        datachain += hour; datachain += " ; "; datachain += minute; datachain += " ; ";
+        datachain += soc; datachain += " ; "; datachain += volts ; datachain += " ; "; 
                 
         // température
         datachain += "RTD : ";
@@ -189,9 +221,41 @@ void setup()
           }
 
 
-      // a faire                                               // enregistrement de la chaine sur carte SD
+      // Affichage ecran
       Serial.print("datachain : "); Serial.println(datachain); //affichage de la chaine complete sur le port serie
-      affichagebase();                                         // Rafraichissement des DATA pour l'affichage pour l'affichage ecran
+
+          // calsal - fonction temporaire juste pour affichage d'une salinité plutot que conductivité
+          float apar=0.0016, bpar=0.6318, cpar=0.5055; // pour un caclul pour une température de 22°
+          //float apar=0.0002, bpar=0.7443, cpar=0.5697; // pour un caclul pour une température de 15°
+          float xval=atof(ecData)/1000;
+          float salfinal = apar*(xval*xval)+bpar*xval-cpar;  
+
+      display.setRotation(3);
+      display.fillScreen(GxEPD_WHITE);
+      
+      //data
+      display.setTextColor(GxEPD_BLACK);
+      display.setFont(f3);
+      display.setCursor(10, 40); display.println(rtdData);
+      display.setCursor(172,40); display.println(salfinal);
+      display.setFont(f2);
+      display.setCursor(40,70);display.print("deg C");
+      display.setCursor(200,70);display.print("PSU");
+    
+      //cadre
+      display.fillRect(147, 0, 2, 90, GxEPD_BLACK);
+      display.fillRect(0, 90, 296, 2, GxEPD_BLACK); 
+      
+      //Last update
+      display.setFont(f1);
+      display.setCursor(0,108); display.print(date);display.print("/");display.print(month);display.print("/");display.print(year);  //date
+      display.setCursor(123,108);display.print(hour);display.print("h");display.print(minute);                                       //heure
+      display.setCursor(205,108);display.print("Bat:");display.print(soc);display.print("%");                                        //batterie
+      display.setCursor(0, 125); display.print("Lat:");display.print(gps.location.lat(), 5);                                                     //lattitude
+      display.setCursor(148,125); display.print("Lng:");display.print(gps.location.lng(), 4);                                                   //longitude
+     
+      display.update();
+
 
 //      digitalWrite(rtdpin, LOW);   // temp
 //      digitalWrite(ecpin, LOW);   // ec
@@ -209,6 +273,7 @@ void setup()
 void loop()
 {
  //never use because of the sleeping of the princess esp32
+ // peut être à utiliser pour la fonction wifi
 }
 
 
@@ -218,75 +283,27 @@ void loop()
  */
 
 
-void affichagebase() {
+
+
+void affichageintro(){                     // texte intro à l'allumage
+  display.setRotation(3);
   display.fillScreen(GxEPD_WHITE);
   
-  //Bandeau AE
-  display.fillRect(0, 0, display.width(), 40, GxEPD_BLACK);
-  display.setCursor(0, 0);
-  display.setTextColor(GxEPD_WHITE);
+  //titre
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f4);
+  display.setCursor(25, 55  );
+  display.println("SENSOCEAN ");
+
+  //version
   display.setFont(f1);
-  display.println();
-  display.println(" Astrolabe Expeditions     SensOcean");
-
-  // Last Update
-  display.setTextColor(GxEPD_BLACK);
-  display.setCursor(0, 60);
-  display.println("Last Update");
-  display.println(" ");
-  //display.print(date);display.print("/");display.print(month);display.print("/20");display.println(year);
-  display.println("Long :xx.3456789 - lat :yyy.3456789 ");
-  
-  //cadre des data
-  display.fillRect(0, 150, display.width(), 30, GxEPD_BLACK);
-  display.fillRect(199, 150, 2, 30, GxEPD_WHITE); 
-  display.fillRect(199, 180, 2, 120, GxEPD_BLACK); 
-  display.setTextColor(GxEPD_WHITE); 
-  display.setCursor(35, 169);
-  display.println("TEMPERATURE");
-  display.setCursor(258, 169);
-  display.println("SALINITY");
-
-  //Data
-  display.setTextColor(GxEPD_BLACK);
-  display.setFont(f3);
-  display.setCursor(20,240);
-  display.print(rtdData);display.print(" C");
-  display.setCursor(210,240);
-  display.print(ecData);display.print(" PSU");
-   
+  display.setCursor(30, 90);
+  display.print("Num Serie :");display.println(numserie);
+  display.setCursor(30, 110);
+  display.print("Ver Soft  :");display.print(versoft);
+ 
   display.update();
 }
-
-
-
-void affichageintro() {
-    display.fillScreen(GxEPD_WHITE);
-
-  // Texte AE
-  display.setTextColor(GxEPD_BLACK);
-  display.setCursor(50, 80);
-  display.setFont(f2);
-  display.println("Astrolabe Expeditions");
-  
-  //cadre des data
-  display.fillRect(0, 140, display.width(), 50, GxEPD_BLACK);
-//  display.fillRect(199, 150, 2, 30, GxEPD_WHITE); 
-//  display.fillRect(199, 180, 2, 120, GxEPD_BLACK); 
-  display.setTextColor(GxEPD_WHITE); 
-  display.setCursor(140, 169);
-  display.println("SENSOCEAN");
-
-  //version 
-  display.setTextColor(GxEPD_BLACK);
-  display.setCursor(50, 250);
-  display.setFont(f1);
-  display.print("Num Serie :");display.print(numserie);
-  display.setCursor(50, 270);
-  display.print("Version :");display.print(versoft);
-  display.update();
-}
-
 
 
 void mesureRTD(){
@@ -374,3 +391,131 @@ void mesureEC(){
 //  Serial.println(sg);                 //this is the specific gravity.
 
 }
+
+void setupBQ27441(void)
+{
+  // Use lipo.begin() to initialize the BQ27441-G1A and confirm that it's
+  // connected and communicating.
+  if (!lipo.begin()) // begin() will return true if communication is successful
+  {
+  // If communication fails, print an error message and loop forever.
+    Serial.println("Error: Unable to communicate with BQ27441.");
+    Serial.println("  Check wiring and try again.");
+    Serial.println("  (Battery must be plugged into Battery Babysitter!)");
+    while (1) ;
+  }
+  Serial.println("Connected to BQ27441!");
+  
+  // Uset lipo.setCapacity(BATTERY_CAPACITY) to set the design capacity
+  // of your battery.
+  lipo.setCapacity(BATTERY_CAPACITY);
+}
+
+static void smartDelay(unsigned long ms)               
+{
+  unsigned long start = millis();
+  do
+  {
+    while (Serial1.available())
+      gps.encode(Serial1.read());
+  } while (millis() - start < ms);
+}
+
+
+
+//void affichageintro() {                 // pour ecran 4,2"
+//    display.fillScreen(GxEPD_WHITE);
+//
+//  // Texte AE
+//  display.setTextColor(GxEPD_BLACK);
+//  display.setCursor(50, 80);
+//  display.setFont(f2);
+//  display.println("Astrolabe Expeditions");
+//  
+//  //cadre des data
+//  display.fillRect(0, 140, display.width(), 50, GxEPD_BLACK);
+////  display.fillRect(199, 150, 2, 30, GxEPD_WHITE); 
+////  display.fillRect(199, 180, 2, 120, GxEPD_BLACK); 
+//  display.setTextColor(GxEPD_WHITE); 
+//  display.setCursor(140, 169);
+//  display.println("SENSOCEAN");
+//
+//  //version 
+//  display.setTextColor(GxEPD_BLACK);
+//  display.setCursor(50, 250);
+//  display.setFont(f1);
+//  display.print("Num Serie :");display.print(numserie);
+//  display.setCursor(50, 270);
+//  display.print("Version :");display.print(versoft);
+//  display.update();
+//}
+
+
+
+
+//void affichagebase() {                                     // pour affichage sur ecran 4,2"
+//  display.fillScreen(GxEPD_WHITE);
+//  // Read battery stats from the BQ27441-G1A
+//  unsigned int soc = lipo.soc();  // Read state-of-charge (%)
+////  unsigned int volts = lipo.voltage(); // Read battery voltage (mV)
+////  int current = lipo.current(AVG); // Read average current (mA)
+////  unsigned int fullCapacity = lipo.capacity(FULL); // Read full capacity (mAh)
+////  unsigned int capacity = lipo.capacity(REMAIN); // Read remaining capacity (mAh)
+////  int power = lipo.power(); // Read average power draw (mW)
+////  int health = lipo.soh(); // Read state-of-health (%)
+////
+////  // Now print out those values:
+////  String toPrint = String(soc) + "% | ";
+////  toPrint += String(volts) + " mV | ";
+////  toPrint += String(current) + " mA | ";
+////  toPrint += String(capacity) + " / ";
+////  toPrint += String(fullCapacity) + " mAh | ";
+////  toPrint += String(power) + " mW | ";
+////  toPrint += String(health) + "%";
+////  Serial.println(toPrint);
+//  
+//  //Bandeau AE
+//  display.fillRect(0, 0, display.width(), 40, GxEPD_BLACK);
+//  display.setCursor(0, 0);
+//  display.setTextColor(GxEPD_WHITE);
+//  display.setFont(f1);
+//  display.println();
+//  display.println(" Astrolabe Expeditions     SensOcean");
+//
+//  // Last Update
+//  display.setTextColor(GxEPD_BLACK);
+//  display.setCursor(0, 60);
+//  display.println("Last Update");
+//  display.println(" ");
+//  //display.print(date);display.print("/");display.print(month);display.print("/20");display.println(year);
+//  display.println("Long :xx.3456789 - lat :yyy.3456789 ");
+//  display.setCursor(258,60);display.print("Bat : "); display.print(soc);
+//    
+//  //cadre des data
+//  display.fillRect(0, 150, display.width(), 30, GxEPD_BLACK);
+//  display.fillRect(199, 150, 2, 30, GxEPD_WHITE); 
+//  display.fillRect(199, 180, 2, 120, GxEPD_BLACK); 
+//  display.setTextColor(GxEPD_WHITE); 
+//  display.setCursor(35, 169);
+//  display.println("TEMPERATURE");
+//  display.setCursor(258, 169);
+//  display.println("SALINITY");
+//
+//  // calsal - fonction temporaire juste pour affichae
+// float apar=0.0016, bpar=0.6318, cpar=0.5055; // pour un caclul pour une température de 22°
+// //float apar=0.0002, bpar=0.7443, cpar=0.5697; // pour un caclul pour une température de 15°
+// float ecfloat=atof(ecData);
+// float xval=ecfloat/1000;
+// float salfinal = apar*(xval*xval)+bpar*xval-cpar;  
+// Serial.print("Ecdata : ");Serial.print(ecData); Serial.print("Ecfloat : ");Serial.print(ecfloat); Serial.print("sal final : ");Serial.println(salfinal);
+//  
+//  //Data
+//  display.setTextColor(GxEPD_BLACK);
+//  display.setFont(f3);
+//  display.setCursor(20,240);
+//  display.print(rtdData);display.print(" C");
+//  display.setCursor(210,240);
+//  display.print(salfinal,2);display.print(" PSU");
+//   
+//  display.update();
+//}
